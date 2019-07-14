4a4310acf7297b9497f7f5d2da6d74ddf5271320
shmoss <shmoss@wisc.edu>
2019-07-14 16:57:12 -0700

    test mouseover

--- js/main.js
+++ js/main.js
@@ -242,6 +242,21 @@ combineArray(sf_events);
         .attr("r", 7.5)
         .style("display", initialDateMatch)
         .style("pointer-events", "all")
+        .on("mouseover", function(d) { 
+         
+            var value2014 = currentMap.get(d.location);     
+                  LeafletDiv.transition()        
+                     .duration(200)      
+                    .style("opacity", .9);
+
+                  LeafletDiv .html('<br/>' + '<b>'+d.Address+'</b>' + '<br/>'+d.Artist
+                    + '<br/>'+d.Date + '<br/>' +d.Venue +'<br/>' + d.OtherInfo + '<br/>' + d.ArtistBio
+                    )
+                    .style("left", (d3.event.pageX+ 15) + "px")     
+                    .style("top", (d3.event.pageY - 150) + "px")
+                    .style("text-align", 'left'); 
+                   d3.select(this).attr("class","countyHover");   
+              })
 
     var todaysDate = new Date 
 
