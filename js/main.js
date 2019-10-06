98ad30342740cccb01e06573b4d6fdea6f42a32a
shmoss <shmoss@wisc.edu>
2019-10-06 15:32:31 -0700

    disable hover on scroll; test

--- js/main.js
+++ js/main.js
@@ -1087,7 +1087,22 @@ function moveLabel(){
             "left": x + "px",
             "top": y + "px"
         });
-};    
+}; 
+
+
+var body = document.body,
+    timer;
+
+window.addEventListener('scroll', function() {
+  clearTimeout(timer);
+  if(!body.classList.contains('disable-hover')) {
+    body.classList.add('disable-hover')
+  }
+
+  timer = setTimeout(function(){
+    body.classList.remove('disable-hover')
+  },500);
+}, false);   
 
 
 
