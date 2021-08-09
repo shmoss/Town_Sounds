098d4cdb06907fb3f57c5a7a4bba72a5b63314d1
shmoss <shmoss@wisc.edu>
2021-08-09 13:27:00 -0500

    test - link removed popup

--- js/main.js
+++ js/main.js
@@ -1020,7 +1020,7 @@ document.addEventListener('DOMContentLoaded', function(e) {
 
 
                     var popInfo = '<br/>' + "<img src='"+d.ArtistImage+"''width='300px' height = '150px'>" + '<br/>'+ '<br/>'+ '<b>'+ '<font size="3em">'+d.Artist.fontcolor( "#ffba00")+ '</font>'+ '</b>'  + '<br/>'+d.EventDate
-                    + '<br/>'+d.Time + '<br/>' + d.Venue + '<br/>' +d.Address +  '<br/>' + '<a href="https://google.com">This is a link to google</a>' + '<br/>' +'<br/>'+ '<b>'+"Genre: &nbsp".fontcolor( "#ffba00")+ '</b>' + d.Genre + '<p>' + '</p>' + '<b>'+" Info: &nbsp".fontcolor( "#ffba00") + '</b>'+d.otherInfo + '<p>' + '</p>' +'<b>'+"Artist Bio: &nbsp".fontcolor( "#ffba00") + '</b>'+d.moreBioInfo+ '<br/>'
+                    + '<br/>'+d.Time + '<br/>' + d.Venue + '<br/>' +d.Address +  '<br/>' + '<br/>' +'<br/>'+ '<b>'+"Genre: &nbsp".fontcolor( "#ffba00")+ '</b>' + d.Genre + '<p>' + '</p>' + '<b>'+" Info: &nbsp".fontcolor( "#ffba00") + '</b>'+d.otherInfo + '<p>' + '</p>' +'<b>'+"Artist Bio: &nbsp".fontcolor( "#ffba00") + '</b>'+d.moreBioInfo+ '<br/>'
                  
 
 
@@ -1134,7 +1134,7 @@ document.addEventListener('DOMContentLoaded', function(e) {
 
                  
                     var popInfo = '<br>' + "<img src='"+d.ArtistImage+"''width='300px' height = '150px'>" + '<br>'+ '<br>'+ '<b>'+ '<font size="3em">'+d.Artist.fontcolor( "#ffba00")+ '</font>'+ '</b>'  + '<br/>'+d.EventDate
-                    + '<br>'+d.Time + '<br>' + d.Venue + '<br>' +d.Address +  '<br>' + '<a href="https://google.com">This is a link to google</a>' + '<br>' +'<br>'+ '<b>'+"Genre: &nbsp".fontcolor( "#ffba00")+ '</b>' + d.Genre + '<p>' + '</p>' + '<b>'+" Info: &nbsp".fontcolor( "#ffba00") + '</b>'+d.otherInfo + '<p>' + '</p>' +'<b>'+"Artist Bio: &nbsp".fontcolor( "#ffba00") + '</b>'+d.moreBioInfo+ '<br>'
+                    + '<br>'+d.Time + '<br>' + d.Venue + '<br>' +d.Address +  '<br>' + '<br>' +'<br>'+ '<b>'+"Genre: &nbsp".fontcolor( "#ffba00")+ '</b>' + d.Genre + '<p>' + '</p>' + '<b>'+" Info: &nbsp".fontcolor( "#ffba00") + '</b>'+d.otherInfo + '<p>' + '</p>' +'<b>'+"Artist Bio: &nbsp".fontcolor( "#ffba00") + '</b>'+d.moreBioInfo+ '<br>'
                  
                  
                     appendText.push(popInfo+ '<br/>' + '<br/>')
@@ -1223,7 +1223,7 @@ document.addEventListener('DOMContentLoaded', function(e) {
 
                  
                     var popInfo = '<br>' + "<img src='"+d.ArtistImage+"''width='300px' height = '150px'>" + '<br>'+ '<br>'+ '<b>'+ '<font size="3em">'+d.Artist.fontcolor( "#ffba00")+ '</font>'+ '<b>'  + '<br>'+d.EventDate
-                    + '<br>'+d.Time + '<br>' + d.Venue + '<br>' +d.Address +  '<br>' + '<a href="https://google.com">This is a link to google</a>' + '<br>' +'<br>'+ '<b>'+"Genre: &nbsp".fontcolor( "#ffba00")+ '</b>' + d.Genre + '<p>' + '</p>' + '<b>'+" Info: &nbsp".fontcolor( "#ffba00") + '</b>'+d.otherInfo + '<p>' + '</p>' +'<b>'+"Artist Bio: &nbsp".fontcolor( "#ffba00") + '</b>'+d.moreBioInfo+ '<br/>'
+                    + '<br>'+d.Time + '<br>' + d.Venue + '<br>' +d.Address +  '<br>' +  '<br>' +'<br>'+ '<b>'+"Genre: &nbsp".fontcolor( "#ffba00")+ '</b>' + d.Genre + '<p>' + '</p>' + '<b>'+" Info: &nbsp".fontcolor( "#ffba00") + '</b>'+d.otherInfo + '<p>' + '</p>' +'<b>'+"Artist Bio: &nbsp".fontcolor( "#ffba00") + '</b>'+d.moreBioInfo+ '<br/>'
                  
                            
 
